522777d0ff102aba872ab18112f5ffc413a552df
Akash Mishra <akash.mishra20@gmail.com>
2015-10-22 17:54:34 +0200

    Adding more parameter for the httpkit server.

--- src/de/otto/tesla/serving_with_httpkit.clj
+++ src/de/otto/tesla/serving_with_httpkit.clj
@@ -6,17 +6,31 @@
 
 (def default-port 3000)
 
+(defn parser-string-config[config element default-value]
+(get-in config [:config element] default-value))
+
+(defn parser-integer-config[config element default-value]
+  (try
+    (Integer. (parser-string-config config element default-value))
+    (catch NumberFormatException e default-value)))
+
+(defn server-config [config]
+    {:port       (parser-integer-config config :server-port default-port)
+     :ip         (parser-string-config config :server-bind "0.0.0.0")
+     :thread     (parser-integer-config config :server-thread 4)
+     :queue-size (parser-integer-config config :server-queue-size 20000)
+     :max-body   (parser-integer-config config :server-max-body 8388608)
+     :max-len    (parser-integer-config config :max-line 4192)
+     })
+
 (defrecord HttpkitServer [config handler]
   c/Lifecycle
   (start [self]
     (log/info "-> starting httpkit")
-    (let [port (get-in config [:config :server-port] default-port)
-          bind (get-in config [:config :server-bind] "0.0.0.0")
+    (let [server-config (server-config config)
           handlers (handler/handler handler)
-          _ (log/info "Starting httpkit with port " port " and bind " bind ".")
-          server (httpkit/run-server handlers
-                                     {:port (Integer. port)
-                                      :ip   bind})]
+          _ (log/info "Starting httpkit with port " (server-config :port) " and bind " (server-config :bind) ".")
+          server (httpkit/run-server handlers server-config)]
       (assoc self :httpkit server)))
 
   (stop [self]
