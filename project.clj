57236efb9876f0215b02dcbe1a06a287204f1e3d
Carl Düvel <hackbert@users.noreply.github.com>
2016-06-15 11:50:00 +0200

    Update to latest tesla microservice version

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                          log4j
                          commons-logging/commons-logging]
 
-            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.26"]
+            :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.1.32"]
                                                  [com.stuartsierra/component "0.3.1"]]}
                        :dev      {:dependencies [[javax.servlet/servlet-api "2.5"]
                                                  [org.slf4j/slf4j-api "1.7.14"]
