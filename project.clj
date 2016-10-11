7bcfb0d6c5ce539dad67c2134fef06d327c78a75
carl.duevel <carl.duevel@holisticon.de>
2016-10-11 18:00:29 +0200

    Added release config

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-httpkit "1.0.1"
+(defproject de.otto/tesla-httpkit "1.0.2-SNAPSHOT"
             :description "httpkit addon for tesla-microservice."
             :url "https://github.com/otto-de/tesla-httpkit"
             :license {:name "Apache License 2.0"
@@ -12,13 +12,12 @@
 
             :source-paths ["src/clojure"]
             :java-source-paths ["src/java"]
-
+            :lein-release {:deploy-via :clojars}
             :exclusions [org.clojure/clojure
                          org.slf4j/slf4j-nop
                          org.slf4j/slf4j-log4j12
                          log4j
                          commons-logging/commons-logging]
-
             :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.3.36"]
                                                  [com.stuartsierra/component "0.3.1"]]}
                        :dev      {:dependencies [[javax.servlet/servlet-api "2.5"]
@@ -26,5 +25,5 @@
                                                  [ch.qos.logback/logback-core "1.1.7"]
                                                  [ch.qos.logback/logback-classic "1.1.7"]
                                                  [ring-mock "0.1.5"]]
-                                  :plugins      [[lein-ancient "0.6.10"]]}}
+                                  :plugins      [[lein-ancient "0.6.10"][lein-release/lein-release "1.0.9"]]}}
             :test-paths ["test" "test-resources"])
