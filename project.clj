699d64cc0523e455d5495590a268f4a9a8f580e3
Torsten Mangner <tmangner@inoxio.de>
2018-10-11 11:55:29 +0200

    lein-release plugin: preparing 1.0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-httpkit "1.0.2-SNAPSHOT"
+(defproject de.otto/tesla-httpkit "1.0.2"
             :description "httpkit addon for tesla-microservice."
             :url "https://github.com/otto-de/tesla-httpkit"
             :license {:name "Apache License 2.0"
